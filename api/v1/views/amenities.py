22c49bfcfa20787b575aa95256e4d205f7d07a8b
Shimaa Salah <114901288+Shima2Salah@users.noreply.github.com>
2024-04-01 23:06:25 +0200

    Airbnb

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -16,25 +16,23 @@ def amenity_methods(amenity_id=None):
     """
     amenities = storage.all(Amenity)
 
-    # GET REQUESTS
     if request.method == 'GET':
-        if not amenity_id: # if no id specified, return all
+        if not amenity_id:
             return jsonify([obj.to_dict() for obj in amenities.values()])
 
         key = 'Amenity.' + amenity_id
-        try: # if obj exists in dictionary, convert from obj -> dict -> json
+        try:
             return jsonify(amenities[key].to_dict())
         except KeyError:
-            abort(404) # Amenity with amenity_id does not exist
+            abort(404)
 
-    # DELETE REQUESTS
     elif request.method == 'DELETE':
         try:
             key = 'Amenity.' + amenity_id
             storage.delete(amenities[key])
             storage.save()
             return jsonify({}), 200
-        except:
+        except KeyError:
             abort(404)
 
     # POST REQUESTS
@@ -45,22 +43,19 @@ def amenity_methods(amenity_id=None):
         else:
             abort(400, 'Not a JSON')
 
-        # instantiate, store, and return new Amenity object
         if 'name' in body_request:
             new_amenity = Amenity(**body_request)
             storage.new(new_amenity)
             storage.save()
             return jsonify(new_amenity.to_dict()), 201
-        else: # if request does not contain required attribute
+        else:
             abort(400, 'Missing name')
 
-    # PUT REQUESTS
     elif request.method == 'PUT':
         key = 'Amenity.' + amenity_id
         try:
             amenity = amenities[key]
 
-            # convert JSON request to dict
             if request.is_json:
                 body_request = request.get_json()
             else:
@@ -75,7 +70,5 @@ def amenity_methods(amenity_id=None):
         except KeyError:
             abort(404)
 
-    # UNSUPPORTED REQUESTS
     else:
         abort(501)
-
