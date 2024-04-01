0057bc53615fc0128dcaea143421c770161589fb
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-04-01 11:32:10 -0700

    Airbnb

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -22,22 +22,22 @@ def states():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def getstate(state_id=None):
     """Retrieves a State object """
-    s = storage.get("State", state_id)
-    if s is None:
+    state = storage.get("State", state_id)
+    if state is None:
         abort(404)
     else:
-        return jsonify(s.to_json())
+        return jsonify(state.to_dict())
 
 
 @app_views.route('/states/<state_id>',
                  methods=['DELETE'], strict_slashes=False)
 def deletestate(state_id=None):
     """Delete a state by id"""
-    s = storage.get("State", str(state_id))
-    if s is None:
+    state = storage.get("State", str(state_id))
+    if state is None:
         abort(404)
     else:
-        storage.delete(obj)
+        storage.delete(state)
         storage.save()
         return jsonify({}), 200
 
@@ -45,26 +45,26 @@ def deletestate(state_id=None):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def add_state():
     """create a new state"""
-    if not request.json:
+    val = request.json
+    if not val:
         return jsonify({"error": "Not a JSON"}), 400
-    if 'name' not in request.json:
+    if 'name' not in val:
         return jsonify({"error": "Missing name"}), 400
-    val = request.get_json()
     new_state = State(**val)
     new_state.save()
     return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_a_state_with_id(state_id=None):
+def update_state(state_id=None):
     """change state data by id"""
     ans = storage.get("State", str(state_id))
-    if ans:
-        if not request.json:
-            return jsonify({"error": "Not a JSON"}), 400
-        for key, val in request.get_json().items():
-            if key not in ['id', 'created_at', 'updated_at']:
-                setattr(answer, key, val)
-        answer.save()
-        return jsonify(answer.to_dict()), 200
-    abort(404)
+    if ans is None:
+        abort(404)
+    if not request.json:
+        return jsonify({"error": "Not a JSON"}), 400
+    for key, val in request.json.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(ans, key, val)
+    ans.save()
+    return jsonify(ans.to_dict()), 200
